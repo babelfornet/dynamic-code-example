4c4b5097b61ab5d5df2390ded7985407a44d2285
Alberto Ferrazzoli <alberto.ferrazzoli@babelfor.net>
2023-02-21 08:35:06 +0100

    first commit

---
+++ DynamicCode/App.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicCode
+{
+    internal class App
+    {
+        #region Demo Code
+        [Obfuscation(Feature = "msil encryption:id=feature1;source=demo;internal=true;password=1234", Exclude = false)]
+        public void Feature1Demo()
+        {
+            Console.WriteLine("Feature1 Demo");
+            Feature2Demo();
+        }
+
+        [Obfuscation(Feature = "msil encryption:id=feature2;source=demo;internal=true;password=1234", Exclude = false)]
+        public static void Feature2Demo()
+        {
+            Console.WriteLine("Feature2 Demo");
+        }
+
+        #endregion
+
+        #region Retail Code 
+        [Obfuscation(Feature = "msil encryption:id=feature1;source=retail;internal=true;password=4567", Exclude = false)]
+        public static void Feature1Retail()
+        {
+            Console.WriteLine("Feature1 Retail");
+            Feature2Retail();
+        }
+
+        [Obfuscation(Feature = "msil encryption:id=feature2;source=retail;internal=true;password=4567", Exclude = false)]
+        public static void Feature2Retail()
+        {
+            Console.WriteLine("Feature2 Retail");
+        }
+
+        #endregion
+
+        #region Special Code
+        [Obfuscation(Feature = "msil encryption:id=feature1;source=special;internal=false;password=0000", Exclude = false)]
+        public static void Feature1Special()
+        {
+            Console.WriteLine("Feature1 Special");
+            Feature2Special();
+        }
+
+        [Obfuscation(Feature = "msil encryption:id=feature2;source=special;internal=false;password=0000", Exclude = false)]
+        public static void Feature2Special()
+        {
+            Console.WriteLine("Feature2 Special");
+        }
+
+        #endregion
+    }
+}
---
+++ DynamicCode/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
+namespace DynamicCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new App();
+            app.Feature1Demo();
+        }
+
+        [Obfuscation(Feature = "msil encryption get password", Exclude = false)]
+        internal static string GetPassword(string source)
+        {
+            Console.WriteLine($"> Get {source} password");
+
+            if (source == "demo")
+                return "1234";
+
+            string pwd = File.ReadAllText(source + ".txt");
+
+            Console.WriteLine($">> {source} password: {pwd}");
+            return pwd;
+        }
+
+        [Obfuscation(Feature = "msil encryption get stream", Exclude = false)]
+        internal static object GetSourceStream(string source)
+        {
+            Console.WriteLine($"> Get {source} stream");
+
+            if (source == "demo")
+            {
+                string[] sources = { "special", "retail" };
+                foreach (var src in sources)
+                {
+                    if (File.Exists($"{src}.txt"))
+                    {
+                        Console.WriteLine($">> Switch to {src}");
+                        return src;
+                    }
+                }
+
+                return null;
+            }
+            
+            // External sources
+            if (File.Exists(source + ".eil"))
+            {
+                Console.WriteLine($">>> Read {source} stream");
+                return File.OpenRead(source + ".eil");
+            }
+
+            return null;
+        }
+    }
+}
